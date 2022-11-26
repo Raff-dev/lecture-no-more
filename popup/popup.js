95568afa8cc5a7e8ce2f634414cb55c4ea111354
Rafał Łazicki <rafal.lazicki99@gmail.com>
2022-11-26 17:45:45 +0100

    [FIX] Fix formatting

--- popup/popup.js
+++ popup/popup.js
@@ -1,18 +1,17 @@
-
-const update = (button) =>{
-    chrome.storage.sync.get(['enabled'], function(result) {
+const update = (button) => {
+    chrome.storage.sync.get(["enabled"], function (result) {
         let paragraph = document.getElementById("lecture-no-more-text");
         let enabled = !result.enabled;
 
-        chrome.storage.sync.set({enabled: enabled});
+        chrome.storage.sync.set({ enabled: enabled });
         paragraph.innerText = enabled ? "Lecture No More is enabled" : "Lecture No More is disabled";
         button.innerText = enabled ? "DISABLE" : "ENABLE";
     });
-}
+};
 
 const onClick = (event) => {
     update(event.target);
-}
+};
 
 let button = document.getElementById("lecture-no-more-button");
 button.addEventListener("click", onClick);
