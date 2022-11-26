076ac2d191babe4ded33051b378e7fa8a030d6f3
Rafał Łazicki <rafal.lazicki99@gmail.com>
2022-11-26 17:15:33 +0100

    [IMP] Toggle lectures

---
+++ README.md
@@ -0,0 +1,1 @@
+# lecture-no-more
---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var toggle = (enabled) => {
+    let visibility = enabled ? "hidden" : "visible";
+
+    for (let el of document.getElementsByTagName("b")) {
+        if (el.innerHTML === "w") {
+            el.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.visibility =
+                visibility;
+        }
+    }
+};
+
+chrome.storage.sync.get(['enabled'], function(result) {
+    toggle(result.enabled)
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (let key in changes) {
+        if (key === "enabled") {
+            toggle(changes[key].newValue);
+        }
+    }
+})
