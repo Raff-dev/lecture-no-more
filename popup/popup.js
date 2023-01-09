7a904523728a9ac2730cabf0714a4691082cfbe4
Rafał Łazicki <rafal.lazicki99@gmail.com>
2023-01-09 13:57:27 +0100

    [IMP] Toggle Subjects

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,7 @@
 const update = (button, toggle) => {
+    console.log("egegeg");
     chrome.storage.sync.get(["enabled"], function (result) {
-        let paragraph = document.getElementById("lecture-no-more-text");
+        let paragraph = document.getElementById("text");
         let enabled = result.enabled;
 
         if (toggle) {
@@ -17,6 +18,58 @@ const onClick = (event) => {
     update(event.target, true);
 };
 
-let button = document.getElementById("lecture-no-more-button");
+const onCheckboxChange = (event) => {
+    let enabled = event.target.checked;
+    let subject = event.target.value;
+
+    chrome.storage.sync.get(["subjects"], function (result) {
+        let subjects = result.subjects;
+        subjects[subject] = enabled;
+        chrome.storage.sync.set({ subjects: subjects });
+    });
+};
+
+const createCheckboxes = (subjects) => {
+    if (!subjects) {
+        return;
+    }
+
+    let checkboxes = document.getElementById("checkboxes");
+    checkboxes.innerHTML = "";
+
+    for (let subject in subjects) {
+        let container = document.createElement("div");
+        let enabled = subjects[subject];
+        let checkboxId = "checkbox-" + subject;
+
+        let input = document.createElement("input");
+        input.type = "checkbox";
+        input.id = checkboxId;
+        input.value = subject;
+        input.checked = enabled;
+        input.addEventListener("change", onCheckboxChange);
+        container.appendChild(input);
+
+        let label = document.createElement("label");
+        label.setAttribute("for", checkboxId);
+        label.textContent = subject;
+        container.appendChild(label);
+        checkboxes.appendChild(container);
+    }
+};
+
+chrome.storage.sync.get(["subjects"], function (result) {
+    createCheckboxes(result.subjects);
+});
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let key in changes) {
+        if (key === "subjects") {
+            createCheckboxes(changes[key].newValue);
+        }
+    }
+});
+
+let button = document.getElementById("buttonqwe");
 button.addEventListener("click", onClick);
 update(button, false);
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,36 @@
-var toggle = (enabled) => {
+var getClassTiles = () => {
+    let elements = document.getElementsByClassName("tdFormList1DSheTeaGrpHTM4");
+    let index = 0;
+    let tiles = {};
+
+    for (let element of elements) {
+        let subjectElement = element.children[0].children[0];
+        if (!subjectElement) {
+            continue;
+        }
+
+        // every other element is teachers name
+        if (index % 2 === 0) {
+            let subject = subjectElement.innerText;
+            let type = element.children[0].children[2].innerText;
+            let id = subject + "_" + type;
+
+            if (subject) {
+                if (!tiles[id]) {
+                    tiles[id] = [element];
+                } else {
+                    tiles[id].push(element);
+                }
+            }
+        }
+        index++;
+    }
+    return tiles;
+};
+
+const tiles = getClassTiles();
+
+var toggleLectures = (enabled) => {
     let visibility = enabled ? "hidden" : "visible";
 
     for (let el of document.getElementsByTagName("b")) {
@@ -9,14 +41,47 @@ var toggle = (enabled) => {
     }
 };
 
-chrome.storage.sync.get(["enabled"], function (result) {
-    toggle(result.enabled);
-});
-
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-    for (let key in changes) {
-        if (key === "enabled") {
-            toggle(changes[key].newValue);
+var toggleTiles = (subjects) => {
+    for (let subject in subjects) {
+        let enabled = subjects[subject];
+        let visibility = enabled ? "visible" : "hidden";
+        for (let tile of tiles[subject]) {
+            tile.parentElement.parentElement.parentElement.parentElement.parentElement.style.visibility = visibility;
         }
     }
+};
+
+var listenChrome = (target, callback) => {
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+        for (let key in changes) {
+            if (key === target) {
+                callback(changes[key].newValue);
+            }
+        }
+    });
+};
+
+var updateMissingSubjects = () => {
+    chrome.storage.sync.get(["subjects"], function (result) {
+        let subjectsSaved = result.subjects || {};
+        let newSubjects = {};
+
+        console.log(subjectsSaved);
+        for (let subject in tiles) {
+            if (subject in subjectsSaved) {
+                newSubjects[subject] = subjectsSaved[subject];
+            } else {
+                newSubjects[subject] = true;
+            }
+        }
+        chrome.storage.sync.set({ subjects: newSubjects });
+    });
+};
+
+chrome.storage.sync.get(["enabled"], function (result) {
+    toggleLectures(result.enabled);
 });
+
+listenChrome("enabled", toggleLectures);
+listenChrome("subjects", toggleTiles);
+updateMissingSubjects();
